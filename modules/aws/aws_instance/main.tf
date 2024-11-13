98381ce1e55e649c8c6e2f1061d934e875345696
Daniel Hardiman <38153501+Muaykhao86@users.noreply.github.com>
2024-11-13 17:48:50 +0000

    Update main.tf

--- modules/aws/aws_instance/main.tf
+++ modules/aws/aws_instance/main.tf
@@ -8,7 +8,7 @@ locals {
 
 
 resource "aws_instance" "instance" {
-  count         = var.instances
+      count         = var.instances
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
