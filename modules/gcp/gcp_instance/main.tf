dc8f01b5820136a88698e58c20ef5bfdd5136b6c
Daniel Hardiman <38153501+Muaykhao86@users.noreply.github.com>
2024-11-19 16:35:53 +0000

    Update main.tf

--- modules/gcp/gcp_instance/main.tf
+++ modules/gcp/gcp_instance/main.tf
@@ -6,7 +6,7 @@ resource "google_compute_instance" "vm" {
   count                   = var.instances
   name                    = "${var.name}-${count.index}"
   machine_type            = var.machine_type
-  zone                    = var
+  zone                    = var.zone
   tags                    = [var.env_tag]
   metadata_startup_script = file("../scripts/startup.sh")
 
