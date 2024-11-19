7611aac061e0fbc1c77ae4a8982656f2c1c64308
Daniel Hardiman <38153501+Muaykhao86@users.noreply.github.com>
2024-11-19 16:38:59 +0000

    Update outputs.tf

--- modules/gcp/gcp_instance/outputs.tf
+++ modules/gcp/gcp_instance/outputs.tf
@@ -1,9 +1,9 @@
 output "ip" {
-  value = resource.google_compute_instance.vm.network_interface.0.network_ip
+  value = resource.google_compute_instance.*.vm.network_interface.0.network_ip
 }
 
 output "ext_ip" {
-  value = resource.google_compute_instance.vm.network_interface.0.access_config.0.nat_ip
+  value = resource.google_compute_instance.*.vm.network_interface.0.access_config.0.nat_ip
 }
 
 output "instance_name" {
