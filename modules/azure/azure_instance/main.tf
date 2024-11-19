5709c8a3e71b800575f92a2becc2a6a9319dc63c
Daniel Hardiman <38153501+Muaykhao86@users.noreply.github.com>
2024-11-19 16:34:02 +0000

    Update main.tf

--- modules/azure/azure_instance/main.tf
+++ modules/azure/azure_instance/main.tf
@@ -66,9 +66,10 @@ resource "azurerm_virtual_machine" "vm" {
 
   os_profile_linux_config {
     disable_password_authentication = false
-    ssh_keys {
-      path     = "/home/clouduser/.ssh/authorized_keys"
-      key_data = file("~/.ssh/id_rsa.pub")
-    }
+#    ssh_keys {
+ #     path     = "/home/clouduser/.ssh/authorized_keys"
+  #    key_data = file("~/.ssh/id_rsa.pub")
+   # }
+
   }
 }
